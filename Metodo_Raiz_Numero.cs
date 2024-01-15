ed50d3c9f59de6ea139b458612f21e05a32328ba
andrioli22 <134887295+andrioli22@users.noreply.github.com>
2024-01-15 11:28:56 -0300

    Create Metodo_Raiz_Numero.cs

---
+++ Metodo_Raiz_Numero.cs
@@ -0,0 +1,19 @@
+ static void Main(string[] args)
+        {
+            double a = 10;
+           
+            double raiz = m (a);
+            Console.WriteLine(raiz);
+                
+            
+        }
+
+
+        public static double m (double x)
+        {
+            double raiz = (Math.Sqrt(x)); ;
+            return raiz;
+
+
+
+        }
