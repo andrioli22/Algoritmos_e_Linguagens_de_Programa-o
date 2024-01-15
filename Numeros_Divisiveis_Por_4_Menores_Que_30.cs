8eff3f521f077f96a9b7a5d5a66a470987411f6c
andrioli22 <134887295+andrioli22@users.noreply.github.com>
2024-01-15 11:10:41 -0300

    Create Numeros_Divisiveis_Por_4_Menores_Que_30.cs

---
+++ Numeros_Divisiveis_Por_4_Menores_Que_30.cs
@@ -0,0 +1,15 @@
+{
+            int x = 1;
+            while (x < 30)
+            {
+                x++;
+                if (x%4==0)
+                {
+                    Console.WriteLine(x);
+                }
+            }
+          
+           
+            
+
+        }
