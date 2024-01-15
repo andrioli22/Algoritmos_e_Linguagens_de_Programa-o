5fa8957743577f15ecfb5c93707477a2e6f78ed3
andrioli22 <134887295+andrioli22@users.noreply.github.com>
2024-01-15 11:05:22 -0300

    Create Soma_Números_1-50.cs

---
+++ Soma_Números_1-50.cs
@@ -0,0 +1,12 @@
+int x = 1, soma = 0;
+            while (x <= 50)
+            {
+                if (x % 2 == 0)
+                {
+                    soma = soma + x;
+                }
+                x++;
+                Console.WriteLine(soma);
+
+
+            }
