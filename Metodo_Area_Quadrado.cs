f097f0b7167f93da7f9d4c7659774e5d761dd5e4
andrioli22 <134887295+andrioli22@users.noreply.github.com>
2024-01-15 11:29:32 -0300

    Create Metodo_Area_Quadrado.cs

---
+++ Metodo_Area_Quadrado.cs
@@ -0,0 +1,19 @@
+    static void Main(string[] args)
+        {
+            double a = 10;
+            double b = 20;
+            double area = m (a, b);
+            Console.WriteLine(area);
+                
+            
+        }
+
+
+        public static double m (double x, double y)
+        {
+            double mult = x * y;
+            return mult;
+
+
+
+        }
