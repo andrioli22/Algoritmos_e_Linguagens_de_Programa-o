cd49560bddfdd4ec795e2e2d127ffa9fa86a39c6
andrioli22 <134887295+andrioli22@users.noreply.github.com>
2024-01-15 11:28:17 -0300

    Create Metodo.cs

---
+++ Metodo.cs
@@ -0,0 +1,18 @@
+ static void Main(string[] args)
+        {
+            double a = 20;
+            double b = 30;
+            double mult = m (a,b);
+            Console.WriteLine(mult);
+            
+        }
+
+
+        public static double m (double x, double y)
+        {
+            double mult = x * y;
+            return mult;
+
+
+
+        }
