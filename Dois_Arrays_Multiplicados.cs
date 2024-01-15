06c757180a4665da925a8462a5cc3af118213165
andrioli22 <134887295+andrioli22@users.noreply.github.com>
2024-01-15 11:25:46 -0300

    Create Dois_Arrays_Multiplicados.cs

---
+++ Dois_Arrays_Multiplicados.cs
@@ -0,0 +1,20 @@
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		int x;
+		int[]a = new int[10];
+		for (int i=0;i<11;i++)
+		{
+			Console.WriteLine("digite o valor");
+			a[i] = int.Parse(Console.ReadLine());
+		}
+		Console.WriteLine("digite um numero");
+		x = int.Parse(Console.ReadLine());
+		int[]m = new int[10];
+		
+			
+	}
+}
